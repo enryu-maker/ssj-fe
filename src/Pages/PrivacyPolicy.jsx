b8c08f40db2ede5c2a9d43764d6dc7aa00325826
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-05-24 01:31:08 +0530

    privacy page completed

--- src/Pages/PrivacyPolicy.jsx
+++ src/Pages/PrivacyPolicy.jsx
@@ -7,105 +7,122 @@ const PrivacyPolicy = () => {
         Privacy Policy
       </h1>
       <div className='flex flex-col gap-5 items-start p-5 w-[80vw]  bg-zinc-50'>
-        <p className='text-xs text-left'>
-          Titan Company Limited (“Titan”, “We”, “Us”) is committed to the
-          compliance of applicable Privacy Laws in the country where Titan’s
-          product is distributed. Protecting the privacy and security of your
-          personal data is of prime importance to Titan, therefore, we conduct
-          our business by abiding by the laws applicable on data privacy and
-          data security in the country where Titan’s product is distributed to
-          you.
+        <p className='text-sm text-left'>
+          Shraddha Jeweller’s eCommerce website is
+          saishraddhajewellers@gmail.com ("us", "we", or "our") operates the
+          saishraddhajewellers@gmail.com website (the "Service").{' '}
         </p>
-        <p className='text-xs text-left'>
-          This Privacy Policy is intended to inform you about the personal data
-          that is collected from you, how your personal data may be used and
-          disclosed, how you can control the use of your personal data, and how
-          your personal data is protected when you use the Titan Application or
-          the application developed by a third party for Titan (“Application”).
-          We do not sell trade your personally identifiable information;
-          however, we may share the same with third parties who assist us in
-          conducting our business, operating our website/Application, or
-          servicing you, so long as those parties agree to keep this information
-          confidential.
+        <p className='text-sm text-left'>
+          This page informs you of our policies regarding the collection, use,
+          and disclosure of personal data when you use our Service and the
+          choices you have associated with that data.
         </p>
-        <h1 className=' font-semibold'>
-          What Information is collected about you:
-        </h1>
-        <p className='text-xs text-left'>Information you provide directly:</p>
-        <p className='text-xs text-left'>
-          When you use certain functions provided by the Application, such as
-          when you register for certain services, access, features or services,
-          we may ask you to provide certain personal data such as name, contact
-          number, postal address, email address, username, password etc.
+        <h1 className=' font-semibold'>Information Collection and Use :</h1>
+        <p className='text-sm text-left'>
+          We collect several different types of information for various purposes
+          to provide and improve our Service to you.{' '}
         </p>
-        <p className='text-xs text-left mt-5'>
-          Information that is automatically collected when you visit the
-          Application:
+        <h1 className=' font-semibold'>Types of Data Collected :</h1>
+        <p className='text-sm text-left '>a. Personal Data</p>
+        <p className='text-sm text-left '>b. Usage Data</p>
+        <p className='text-sm text-left '>c. Tracking & Cookies Data</p>
+
+        <h1 className=' font-semibold'>Use of Data :</h1>
+        <p className='text-lg text-left '>
+          We use the collected data for various purposes:
         </p>
-        <p className='text-xs text-left '>
-          a. When you download and visit the application, certain information
-          may be collected automatically, which includes:
+        <ul className=' list-disc flex flex-col gap-4'>
+          <li className=' text-sm'>To provide and maintain our Service </li>
+          <li className=' text-sm'>
+            To notify you about changes to our Service
+          </li>
+          <li className=' text-sm'>
+            To allow you to participate in interactive features of our Service
+            when you choose to do so
+          </li>
+          <li className=' text-sm'>To provide customer support</li>
+          <li className=' text-sm'>
+            To gather analysis or valuable information so that we can improve
+            our Service
+          </li>
+          <li className=' text-sm'>To monitor the usage of our Service</li>
+          <li className=' text-sm'>
+            To detect, prevent and address technical issues
+          </li>
+        </ul>
+        <h1 className=' font-semibold'>Transfer of Data</h1>
+        <p className='text-sm'>
+          Your information, including Personal Data, may be transferred to our
+          databank and maintained on our computers located outside of your
+          state, province, country, or other governmental jurisdiction where the
+          data protection laws may differ from those of your jurisdiction.
         </p>
-        <p className='text-xs text-left '>
-          b. Your computer’s Internet Protocol (IP) Address,
+        <h1 className=' font-semibold'>Disclosure of Data</h1>
+        <p className='text-sm'>Legal Requirements</p>
+        <p className='text-sm'>To comply with a legal obligation </p>
+        <p className='text-sm'>
+          To protect and defend the rights or property of [Your Company Name]
         </p>
-        <p className='text-xs text-left '>
-          c. Your browser type and operating system,
-        </p>
-        <p className='text-xs text-left '>
-          d. Web pages and links you visit, advertisements you view,
-        </p>
-        <p className='text-xs text-left '>
-          e. Your bandwidth speed and information about the software programs
-          that are installed on your computer,
-        </p>
-        <p className='text-xs text-left '>f. Standard server log information</p>
-        <p className='text-xs text-left '>
-          g. Geo Location Information: When you use our app SSJ or any related
-          services, we may collect information about your geolocation via third
-          party services to notify you with customized offers. Information that
-          we collect from other sources: We may receive information about you
-          from publicly and commercially available sources (as permitted by
-          law), which we may combine with other information we receive from or
-          about you. We also may receive information about you from third-party
-          social networking services when you choose to connect with those
-          services.
+        <h1 className=' font-semibold'>Security of Data</h1>
+        <p className=' text-sm'>
+          The security of your data is important to us, but remember that no
+          method of transmission over the Internet{' '}
         </p>
 
-        <h1 className=' font-semibold'>
-          How we use the information we collect:
-        </h1>
-        <p className='text-xs text-left '>
-          General: Titan may use the information collected from you to register
-          you or your device for a Service, to provide a service or feature you
-          request, to provide customized content and make recommendations based
-          on your past activities on our Application.
+        <p className=' text-sm'>
+          Terms and Condition & privacy policy
+          <br />
+          Refund and cancelation policy
+          <br /> Shipping and delivery policy? Payment Terms?{' '}
         </p>
-        <p className='text-xs text-left '>
-          Advertisements: Titan may use your information to provide you with
-          product and service updates, newsletters and other communications
-          about existing and/or new products and services by post, email,
-          telephone, in-device messaging and/or text message (SMS), if you have
-          provided your prior consent or we are otherwise permitted to do so
-          under applicable law.
-        </p>
-        <p className='text-xs text-left '>
-          Statistics and Research: Titan may use your information to create user
-          group profiles or segment data and to otherwise create anonymous,
-          aggregated statistics about the use of our products, services and
-          loyalty programs, which we may share with third parties and/or make
-          available to the public.
-        </p>
-        <p className='text-xs text-left '>
-          Product/Services Improvement: Titan may use your information to
-          improve and enhance existing Titan products, services and Applications
-          and develop new offerings, recommendations, advertisementsand other
-          communications and learn more about customers’ preferences in general.
-          Publish your reviews, comments and content: Where you have uploaded
-          product reviews, comments or content to our websites or Application
-          and made them publicly visible, Titan may link to, publish or
-          publicize these materials elsewhere including in our own
-          advertisements.
+        <p className='text-xs text-pretty text-wrap'>
+          We have planned cost on Delivery (COD), hence payment will be made
+          against delivery of ordered items. Our authorized person would carry
+          the sealed packing in a nice packing with invoice details. in order to
+          earn good name and fame shall be signing the agreement, to consider
+          refund in case quality or quantity has any issues or consider
+          cancellation of order, if it is conveyed six hour in advance of
+          scheduled delivery in case of tomorrow’s order, or 48 hrs in advance
+          for high value orders, but yes for ATM (any time order) no
+          cancellation is possible at short notice. In case any customer refuses
+          accept delivery and do not pay, we may report to police and file an
+          FIR. Sai Shraddha Jeweller’s eCommerce website is
+          saishraddhajewellers@gmail.com offers a marketplace, for its range of
+          products. The Service includes product specifications pricing, taxes
+          and delivery. We may also help facilitate the resolution of disputes
+          mutually. Sai Shraddha Jeweller’s guarantee the quality, safety,
+          authenticity, or legality of product sold or the services provided.
+          Sai Shraddha Jeweller’s does not control the behaviour of users and
+          reserves the right to delay the completion of any transaction for a
+          reasonable period of time for purposes of fraud detection and
+          otherwise protecting itself from illegal or wrongful activities or
+          other violations of these Terms of Services. Commercial Use: Unless
+          otherwise expressly authorized herein or in the Service, we mutually
+          agree not to display, distribute, license, perform, publish,
+          reproduce, duplicate, copy, create derivative works from, modify,
+          service, or access to the Service. Sai Shraddha Jeweller would arrange
+          delivery themselves or the customer would himself collect, till a
+          delivery module is operational. Finally website will have three
+          provisions, 1- Sai Shraddha Jeweller’s shall deliver, 2- Customer
+          shall collect themselves, 3- Pick and drop in auto mode via online
+          tracking mechanism. Items shall be delivered in nicely packed ready to
+          eat condition. Packaging: Sai Shraddha Jeweller agree to arrange their
+          own packaging materials – ideally in the form of standard sized decent
+          boxes as used by other reputed Jewellers in India. Sai Shraddha
+          Jeweller agree that it is their responsibility to pack and protect the
+          items for delivery and transit appropriately. Sai Shraddha Jeweller’s
+          and its delivery partner will not accept any liability for damages
+          after few hours of delivery, breakage or any other temperature
+          impacted damage on the deliverd item due to any reasons, including
+          directly or indirectly caused by poor packing whatsoever, which
+          contravenes these requirements. Delivery: Chefs and customers agree
+          that while Sai Shraddha Jeweller’s will endeavour to deliver the
+          Delivery Items within the indicated timeframe, any delivery times
+          stated or agreed or represented are estimates only. Delivery times may
+          be affected by a number of factors, including those outside of control
+          of Sai Shraddha Jeweller and its partner’s control such failure of
+          equipment, bad weather and/or traffic conditions, including third
+          Party Distribution Channels.
         </p>
       </div>
     </div>
