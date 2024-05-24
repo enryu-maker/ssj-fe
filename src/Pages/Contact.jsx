342a78f42c19f7bcef07cf615bb5965bf4de7cd0
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-05-24 12:30:09 +0530

    contact page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className='flex flex-col gap-5 justify-center items-center mt-5 font-Raleway'>
+      <h1 className=' text-4xl text-primary-color font-semibold'>
+        Help & Contact
+      </h1>
+      <div className='flex flex-col gap-5 items-start p-5 w-[80vw]  bg-zinc-50'></div>
+    </div>
+  );
 };
 
 export default Contact;
