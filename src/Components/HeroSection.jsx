542db6202e2ee7e622254a40f40f677cb8b69dbe
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-07-01 10:22:15 +0530

    cc

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -46,15 +46,7 @@ const HeroSection = () => {
               src={banner.image} // Adjust according to your API response structure
               alt={banner.context || `Banner ${index + 1}`}
               className='w-full h-full object-fill '
-            />
-            <div className='absolute font-Raleway left-10 right-10 md:top-44 top-10'>
-              <p className='mt-5 md:text-4xl text-3xl font-light md:w-96 text-left text-white'>
-                {banner.context || 'We are selling happiness'}
-              </p>
-              <button className='md:mt-5 mt-2 md:flex uppercase bg-transparent delay-100 transition-all duration-75 ease-linear hover:border-2 text-secondary-color font-semibold hover:text-primary-color py-2 px-4 border border-primary-color rounded-full'>
-                {banner.context || 'Explore More'}
-              </button>
-            </div>
+            /> 
           </SwiperSlide>
         ))}
       </Swiper>
