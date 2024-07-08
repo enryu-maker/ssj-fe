904be9abc19f6c12b4b3211496b3eb54303c2f27
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-07-08 13:50:26 +0530

    cc

--- src/Pages/Categories.jsx
+++ src/Pages/Categories.jsx
@@ -37,7 +37,7 @@ const Categories = () => {
       <h1 className="md:text-4xl text-2xl text-center space-y-10 mt-5  font-semibold text-primary-color uppercase">
         {category.name}
       </h1>
-      <div div className="grid md:grid-cols-4 gap-5 p-5 mt-5">
+      <div div className="grid grid-cols-2 md:grid-cols-4 gap-5 p-5 mt-5">
         {category?.products?.map((item) => (
           <div key={item.id} className="product-card">
             <ProductCard {...item} />
