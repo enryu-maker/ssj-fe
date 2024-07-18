8ed013cfe65aacc9e444326488cd2547c53c7649
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-07-18 10:54:54 +0530

    all mighty push

--- src/data.js
+++ src/data.js
@@ -1,111 +1,102 @@
-
 export const NewForYouData = [
   {
     imgUrl: 'https://i.ibb.co/7StW190/pretty-in-pink-new.jpg"',
   },
 
   {
-    imgUrl: 'https://i.ibb.co/bm85wfc/trending-earrings.webp',
+    imgUrl: "https://i.ibb.co/bm85wfc/trending-earrings.webp",
   },
   {
-    imgUrl: 'https://i.ibb.co/bBZCNCk/modern-designs.webp',
+    imgUrl: "https://i.ibb.co/bBZCNCk/modern-designs.webp",
   },
 ];
 export const ShopByGenderData = [
   {
-    name: 'men',
-    imgUrl: 'https://i.ibb.co/3rfCfvs/Men.webp',
+    name: "men",
+    imgUrl: "https://i.ibb.co/3rfCfvs/Men.webp",
   },
 
   {
-    name: 'kids',
-    imgUrl: 'https://i.ibb.co/BwZG9Wz/kid.jpg',
+    name: "kids",
+    imgUrl: "https://i.ibb.co/BwZG9Wz/kid.jpg",
   },
   {
-    name: 'woman',
-    imgUrl: 'https://i.ibb.co/176sqc7/Woman.webp',
+    name: "woman",
+    imgUrl: "https://i.ibb.co/176sqc7/Woman.webp",
   },
 ];
 
 export const UsefulLinks = [
   {
-    name: 'All jewellery',
-    link: '/products',
+    name: "All jewellery",
+    link: "/products",
   },
   {
-    name: 'Collections',
-    link: '#collections',
+    name: "Collections",
+    link: "#collections",
   },
   {
-    name: 'Orders',
-    link: '/dashboard/orders',
-  },
-  {
-    name: 'Find a Store',
-    link: '/stores',
+    name: "Find a Store",
+    link: "/stores",
   },
 ];
-
 
 export const MoreLink = [
-  
   {
-    name: 'GOLD RATE',
-    link: '',
+    name: "GOLD RATE",
+    link: "",
   },
   {
-    name: 'BOOK AN APPOINTMENT',
-    link: '#contacts',
+    name: "BOOK AN APPOINTMENT",
+    link: "#contacts",
   },
   {
-    name: 'FAQ',
-    link: '',
+    name: "FAQ",
+    link: "",
   },
- 
 ];
-
 
 export const Information = [
   {
-    name: 'Careers',
-    link: '',
+    name: "Careers",
+    link: "",
   },
   {
-    name: 'Help & FAQs',
-    link: '#contacts',
+    name: "Help & FAQs",
+    link: "#contacts",
   },
   {
-    name: 'About SSJ',
-    link: '#about-us',
+    name: "About SSJ",
+    link: "#about-us",
   },
   {
-    name: 'Blogs',
-    link: '#blogs',
+    name: "Blogs",
+    link: "#blogs",
   },
 ];
 export const StoreInCities = [
   {
-    city: 'Mumbai',
-    img: 'https://i.ibb.co/MsG5FVs/Mumbai-42b4bffdd6b38798a062.png',
-    store: '10 stores',
-    link: '',
+    city: "Mumbai",
+    img: "https://i.ibb.co/MsG5FVs/Mumbai-42b4bffdd6b38798a062.png",
+    store: "10 stores",
+    link: "",
   },
   {
-    city: 'Mumbai',
-    img: 'https://i.ibb.co/MsG5FVs/Mumbai-42b4bffdd6b38798a062.png',
-    store: '18 stores',
-    link: '',
+    city: "Mumbai",
+    img: "https://i.ibb.co/MsG5FVs/Mumbai-42b4bffdd6b38798a062.png",
+    store: "18 stores",
+    link: "",
   },
   {
-    city: 'Mumbai',
-    img: 'https://i.ibb.co/MsG5FVs/Mumbai-42b4bffdd6b38798a062.png',
-    store: '6 stores',
-    link: '',
+    city: "Mumbai",
+    img: "https://i.ibb.co/MsG5FVs/Mumbai-42b4bffdd6b38798a062.png",
+    store: "6 stores",
+    link: "",
   },
   {
-    city: 'Mumbai',
-    img: 'https://i.ibb.co/MsG5FVs/Mumbai-42b4bffdd6b38798a062.png',
-    store: '7 stores',
-    link: '',
+    city: "Mumbai",
+    img: "https://i.ibb.co/MsG5FVs/Mumbai-42b4bffdd6b38798a062.png",
+    store: "7 stores",
+    link: "",
   },
 ];
